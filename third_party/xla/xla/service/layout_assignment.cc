b5e67a4d1efc2a5e8fcb85d65dc41ac70dc1c8fe
A. Unique TensorFlower <gardener@tensorflow.org>
2025-09-12 01:25:05 -0700

    [XLA] Add `asin` as a native HLO opcode.
    This change promotes `asin` from a composite operation to a native HLO opcode (`kAsin`). This allows for direct lowering to device-specific intrinsics which should be more performant. Support is added for GPU.
    
    PiperOrigin-RevId: 806177064

--- third_party/xla/xla/service/layout_assignment.cc
+++ third_party/xla/xla/service/layout_assignment.cc
@@ -2845,6 +2845,7 @@ bool LayoutAssignment::InstructionCanChangeLayout(
     case HloOpcode::kAbs:
     case HloOpcode::kAcos:
     case HloOpcode::kAcosh:
+    case HloOpcode::kAsin:
     case HloOpcode::kAdd:
     case HloOpcode::kAddDependency:
     case HloOpcode::kAnd:
